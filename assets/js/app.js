ac118f43c2a80fa864772bfc69128f46b30e7c30
htrammell <hunterttrammell@gmail.com>
2018-09-26 22:47:26 -0500

    firebase integration

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,14 @@
-
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDqKFoezSoE-C-y0Hh-gavtW13oZ64K_Yg",
+    authDomain: "movieswithfriends-1c676.firebaseapp.com",
+    databaseURL: "https://movieswithfriends-1c676.firebaseio.com",
+    projectId: "movieswithfriends-1c676",
+    storageBucket: "movieswithfriends-1c676.appspot.com",
+    messagingSenderId: "305686437446"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database()
 $('#results').on('click', function showMovies(){
     event.preventDefault();
     //removes html from container that will house new data on this page
@@ -23,16 +33,35 @@ $('#results').on('click', function showMovies(){
         //Div to house all images that is linked to output in HTML
         var imgDiv = $('<div>').attr('id', "imgDiv")
         $('#container').append(imgDiv)
+        //creates access code that users will query
+        var genKey = makeId()
+        //assigns that access code to the name of the branch in firebase
+        database.ref(genKey).push({
+            "Access Code": genKey,
+            "Movies": {}
+        })
         //for loop to create 5 posters and place them on page
         for (var i=0;i<5; i++){
+            //varible for poster url
+            var posterURL = "https://image.tmdb.org/t/p/w500" + response.results[i].poster_path
             //gets poster img based on URL
-            var poster = $('<img>').attr('src', "https://image.tmdb.org/t/p/w500" + response.results[i].poster_path)
+            var poster = $('<img>').attr('src', posterURL)
+            //variable to house movie title
+            var movieTitle = response.results[i].title
             //ads ID for styling and data for pushing up to firebase
-            $(poster).attr('id', "poster").attr('data', response.results[i].title)
+            $(poster).attr('id', "poster").attr('data', movieTitle)
             //stores URL in data in addition to SRC
-            $(poster).attr('data-img', "https://image.tmdb.org/t/p/w500" + response.results[i].poster_path)
+            $(poster).attr('data-img', posterURL)
             //links poster to imgDiv created above
             $(imgDiv).append(poster)
+            //pushes the new data up to firebase as it is gen
+            //Exception - will need to modify to allow user to manually add items in
+            database.ref(genKey + '/Movies').push({
+                ["Movie" + i]:{
+                    "Title": movieTitle,
+                    "Poster": posterURL
+                }
+            })
         }
         //creates button that will submit results and trigger firebase call
         var newBtn = $('<button class="btn text-center" type="submit">')
@@ -42,5 +71,19 @@ $('#results').on('click', function showMovies(){
         console.log(response)
         console.log(queryURL)
     })
+
 })
+
+//generates random 5-character user ID - will need to switch to a better system
+//but works for now since after survey is done, data is wiped from firebase
+function makeId() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+    for (var i = 0; i < 5; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return text;
+  }
+
 
